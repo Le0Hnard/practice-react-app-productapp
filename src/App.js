64e63e926834ae176ca0327322936633d0a1ea16
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-10-08 16:11:34 +0800

    Initial app setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import ProductsAndSuppliers from "./components/ProductsAndSuppliers";
+
+export default class App extends Component {
+  render() {
+    return <ProductsAndSuppliers />;
+  }
+}
