4d255cfbc6d8446b58689699ea0122834b18d126
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-10-11 14:05:39 +0800

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
-import ProductsAndSuppliers from "./components/ProductsAndSuppliers";
 import { Provider } from "react-redux";
 import dataStore from "./store";
+import { Selector } from "./components/Selector";
+import { ProductDisplay } from "./components/ProductDisplay";
+import { SupplierDisplay } from "./components/SupplierDisplay";
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={dataStore}>
-        <ProductsAndSuppliers />
+        <Selector>
+          <ProductDisplay name="Products" />
+          <SupplierDisplay name="Suppliers" />
+        </Selector>
       </Provider>
     );
   }
